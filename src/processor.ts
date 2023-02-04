26268068fe7eeaeceb7b8068baacb7ef85d46f1d
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-02-04 17:03:08 +0530

    remove status check while attaching preimage

--- src/processor.ts
+++ src/processor.ts
@@ -9,7 +9,7 @@ const processor = new SubstrateBatchProcessor()
         chain: 'wss://kusama.api.onfinality.io/public-ws',
         archive: lookupArchive('kusama', { release: 'FireSquid' }),
     })
-    .setBlockRange({from: 0})
+    .setBlockRange({from: 9693427})
     .addEvent('Democracy.Proposed', { data: { event: { args: true, extrinsic: { hash: true, } }, } } as const)
     .addEvent('Democracy.Tabled', { data: { event: { args: true, extrinsic: { hash: true, } }, } } as const)
     .addEvent('Democracy.Started', { data: { event: { args: true, extrinsic: { hash: true, } }, } } as const)
