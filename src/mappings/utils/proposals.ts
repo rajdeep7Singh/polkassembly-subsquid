5ee5f41f155a9636ae0980afa2fe40587f0f47eb
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-06-17 11:47:40 +0530

    minor

--- src/mappings/utils/proposals.ts
+++ src/mappings/utils/proposals.ts
@@ -1061,10 +1061,10 @@ export async function sendNotification(ctx: BatchContext<Store, unknown>, propos
         args : {
             network: 'cere',
             postType: type,
-            postId: index || hash,
+            postId: String(index) || hash,
             proposerAddress: proposer || curator,
             newStatus: status,
-            track: trackNumber,
+            track: String(trackNumber),
             statusName: statusName,
           }
     }
