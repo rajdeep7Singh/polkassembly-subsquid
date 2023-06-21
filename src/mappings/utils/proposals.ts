30cf0bf96a47c2fa94f564eb0205415814747974
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-06-21 18:26:29 +0530

    fix

--- src/mappings/utils/proposals.ts
+++ src/mappings/utils/proposals.ts
@@ -1015,7 +1015,7 @@ export async function sendNotification(ctx: BatchContext<Store, unknown>, propos
     const notification = {
         trigger: trigger,
         args : {
-            network: 'kusama',
+            network: 'moonriver',
             postType: type,
             postId: String(index) || hash,
             proposerAddress: proposer || curator,
