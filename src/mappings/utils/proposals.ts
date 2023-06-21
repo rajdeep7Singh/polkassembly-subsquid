8b3535a616445cd44a679769741b5c36775afe09
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-06-21 18:23:27 +0530

    fix

--- src/mappings/utils/proposals.ts
+++ src/mappings/utils/proposals.ts
@@ -2,6 +2,7 @@ import { Store } from '@subsquid/typeorm-store'
 import { toJSON } from '@subsquid/util-internal-json'
 import { BatchContext, SubstrateBlock } from '@subsquid/substrate-processor'
 import { MissingProposalRecordWarn } from '../../common/errors'
+import { NOTIFICATION_URL } from '../../consts/consts'
 
 import {
     MotionThreshold,
@@ -116,6 +117,7 @@ export async function updateProposalStatus(
             proposal,
         })
     )
+    await sendNotification(ctx, proposal, 'proposalStatusChanged')
 }
 async function getProposalId(store: Store, type: ProposalType) {
     const count = await store.count(Proposal, { where: { type } })
@@ -333,6 +335,7 @@ export async function createFellowshipReferendum( ctx: BatchContext<Store, unkno
             proposal,
         })
     )
+    await sendNotification(ctx, proposal, 'newProposalCreated')
     return proposal
 }
 
@@ -375,4 +378,85 @@ export function createDecisionDeposit(data: DecisionDepositData): DecisionDeposi
 
 export function createSubmissionDeposit(data: SubmissionDepositData): SubmissionDeposit {
     return new SubmissionDeposit(toJSON(data))
+}
+export async function sendNotification(ctx: BatchContext<Store, unknown>, proposal: Proposal, trigger: String) {
+    const { hash, type, index, proposer, curator, status, trackNumber } = proposal
+    let statusName = null
+
+    // if difference between proposal update time and current time > 10 mins return
+    if(proposal.updatedAt && (new Date().getTime() - proposal.updatedAt.getTime()) > 600000){
+        ctx.log.info(`Proposal ${index || hash} updated more than 10 mins ago, skipping notification`)
+        return
+    }
+
+    if([ProposalStatus.Started, 
+        ProposalStatus.Submitted, 
+        ProposalStatus.Added, 
+        ProposalStatus.Proposed, 
+        ProposalStatus.Opened,
+    ].includes(status)){
+        statusName = 'submitted'
+    }
+    else if([ProposalStatus.Executed,
+        ProposalStatus.Cancelled,
+        ProposalStatus.Killed,
+        ProposalStatus.Rejected,
+        ProposalStatus.Executed,
+        ProposalStatus.ExecutionFailed,
+        ProposalStatus.Closed,
+        ProposalStatus.Approved,
+        ProposalStatus.Disapproved,
+        ProposalStatus.Awarded,
+        ProposalStatus.Claimed,
+        ProposalStatus.NotPassed,
+        ProposalStatus.Passed,
+        ProposalStatus.Tabled,
+        ProposalStatus.Retracted,
+        ProposalStatus.Slashed,
+        ProposalStatus.TimedOut,
+    ].includes(status)){
+        statusName = 'closed'
+    }
+    else if([ProposalStatus.Deciding,
+        ProposalStatus.ConfirmStarted,
+        ProposalStatus.ConfirmAborted,
+    ].includes(status)){
+        statusName = 'voting'
+    }
+
+    const notification = {
+        trigger: trigger,
+        args : {
+            network: 'polkadot',
+            postType: type,
+            postId: String(index) || hash,
+            proposerAddress: proposer || curator,
+            statusType: statusName,
+            track: String(trackNumber),
+            statusName: status,
+          }
+    }
+
+    if(!process.env.NOTIFICATION_API_KEY){
+        ctx.log.error(`Notification Api Key not found`)
+        return
+    }
+
+    ctx.log.info(`Sending notification with data ${JSON.stringify(notification)}`)
+
+    const response = await fetch(NOTIFICATION_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': process.env.NOTIFICATION_API_KEY || '',
+            'x-source': 'polkassembly'
+        },
+        body: JSON.stringify(notification),
+    })
+
+    ctx.log.info(`Notification response ${JSON.stringify(response)}`)
+    if (response.status !== 200) {
+        ctx.log.error(`Notification failed for proposal ${index || hash} with status ${response.status}`)
+        return
+    }
 }
