46c64de89bd4600f76727ab2a1e6ea87d45b4a30
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-06-22 23:56:15 +0530

    notif tip fix

--- src/mappings/utils/proposals.ts
+++ src/mappings/utils/proposals.ts
@@ -1061,7 +1061,7 @@ export async function sendNotification(ctx: BatchContext<Store, unknown>, propos
         args : {
             network: 'integritee',
             postType: type,
-            postId: String(index) || hash,
+            postId: type != ProposalType.Tip ? String(index) : hash,
             proposerAddress: proposer || curator,
             statusType: statusName,
             track: String(trackNumber),
