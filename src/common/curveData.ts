6f9d14618445a629caeb6602807b230b311cab29
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-06-23 23:39:40 +0530

    curve data fix

--- src/common/curveData.ts
+++ src/common/curveData.ts
@@ -11,19 +11,12 @@ export async function updateCurveData(ctx: BatchContext<Store, unknown>, header:
     if(proposal.index == null || proposal.index == undefined){
         return;
     }
-    // const cd = await ctx.store.get(CurveData, { 
-    //     where: { 
-    //         index: proposal.index,
-    //     },
-    //     order: {
-    //         timestamp: 'DESC',
-    //     },
-    // })
 
-    const blockDateTime = new Date(header.timestamp)
     let approvalPercent = 0.0
     let supportPercent = 0.0
+    
     const storageData = await getStorageData(ctx, proposal.index, header)
+
     if (storageData) {
         const tally = storageData.tally
         const totalIssuance = await getTotalIssuanceStorageData(ctx, header)
@@ -31,17 +24,17 @@ export async function updateCurveData(ctx: BatchContext<Store, unknown>, header:
         const activeIssuance = totalIssuance - inactiveIssuance;
         approvalPercent = Number(tally.ayes) / Number((tally.ayes + tally.nays)) * 100
         supportPercent = Number(tally.support) / Number(activeIssuance) * 100
-    }
 
-    await ctx.store.insert(
-        new CurveData({
-            id: randomUUID(),
-            index: proposal.index,
-            timestamp: new Date(header.timestamp),
-            approvalPercent: approvalPercent,
-            supportPercent: supportPercent,
-            block: header.height,
-        })
-    )
+        await ctx.store.insert(
+            new CurveData({
+                id: randomUUID(),
+                index: proposal.index,
+                timestamp: new Date(header.timestamp),
+                approvalPercent: approvalPercent,
+                supportPercent: supportPercent,
+                block: header.height,
+            })
+        )
+    }
 
 }
