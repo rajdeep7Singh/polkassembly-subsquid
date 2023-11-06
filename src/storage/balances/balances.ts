ee5f773bdcd142c2b2dd0eea3acd5fc1880d4a25
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-11-06 23:46:19 +0530

    another fix

--- src/storage/balances/balances.ts
+++ src/storage/balances/balances.ts
@@ -5,16 +5,16 @@ import { Block, ProcessorContext} from '../../processor'
 
 
 export async function getTotalIssuanceStorageData(ctx: ProcessorContext<Store>, block: Block): Promise<bigint> {
-    if (inactiveIssuance.v2201.is(block)) {
-        return await inactiveIssuance.v2201.get(block) || BigInt(0)
+    if (totalIssuance.v40.is(block)) {
+        return await totalIssuance.v40.get(block) || BigInt(0)
     } else {
         throw new UnknownVersionError('Balances.InactiveIssuance')
     }
 }
 
 export async function getTotalInactiveIssuanceStorageData(ctx: ProcessorContext<Store>, block: Block): Promise<bigint> {
-    if (totalIssuance.v40.is(block)) {
-        return await totalIssuance.v40.get(block) || BigInt(0)
+    if (inactiveIssuance.v2201.is(block)) {
+        return await inactiveIssuance.v2201.get(block) || BigInt(0)
     } else {
         throw new UnknownVersionError('Balances.TotalIssuance')
     }
