0dcd46200e5b1a31a263601d73de93f94e382fed
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-11-06 23:47:38 +0530

    minor fix

--- src/storage/balances/balances.ts
+++ src/storage/balances/balances.ts
@@ -5,17 +5,17 @@ import { Block, ProcessorContext} from '../../processor'
 
 
 export async function getTotalIssuanceStorageData(ctx: ProcessorContext<Store>, block: Block): Promise<bigint> {
-    if (inactiveIssuance.v2201.is(block)) {
-        return await inactiveIssuance.v2201.get(block) || BigInt(0)
-    } else {
-        throw new UnknownVersionError('Balances.InactiveIssuance')
-    }
-}
-
-export async function getTotalInactiveIssuanceStorageData(ctx: ProcessorContext<Store>, block: Block): Promise<bigint> {
     if (totalIssuance.v49.is(block)) {
         return await totalIssuance.v49.get(block) || BigInt(0)
     } else {
         throw new UnknownVersionError('Balances.TotalIssuance')
+    }
+}
+
+export async function getTotalInactiveIssuanceStorageData(ctx: ProcessorContext<Store>, block: Block): Promise<bigint> {
+    if (inactiveIssuance.v2201.is(block)) {
+        return await inactiveIssuance.v2201.get(block) || BigInt(0)
+    } else {
+        throw new UnknownVersionError('Balances.InactiveIssuance')
     }
 }
