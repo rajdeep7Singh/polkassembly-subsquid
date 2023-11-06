105abcc896337372627c3c7a0017b7526cd5f7b4
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-11-07 00:09:12 +0530

    another fix

--- src/storage/balances/balances.ts
+++ src/storage/balances/balances.ts
@@ -1,21 +1,13 @@
 import { Store } from '@subsquid/typeorm-store'
-import { UnknownVersionError } from '../../common/errors'
-import { inactiveIssuance, totalIssuance } from '../../types/balances/storage'
 import { Block, ProcessorContext} from '../../processor'
 
 
 export async function getTotalIssuanceStorageData(ctx: ProcessorContext<Store>, block: Block): Promise<bigint> {
-    if (totalIssuance.v40.is(block)) {
-        return await totalIssuance.v40.get(block) || BigInt(0)
-    } else {
-        throw new UnknownVersionError('Balances.InactiveIssuance')
-    }
+    const storageData = await block._runtime.getStorage(block.hash, 'Balances.TotalIssuance')
+    return storageData || BigInt(0)
 }
 
 export async function getTotalInactiveIssuanceStorageData(ctx: ProcessorContext<Store>, block: Block): Promise<bigint> {
-    if (inactiveIssuance.v2201.is(block)) {
-        return await inactiveIssuance.v2201.get(block) || BigInt(0)
-    } else {
-        throw new UnknownVersionError('Balances.TotalIssuance')
-    }
+    const storageData = await block._runtime.getStorage(block.hash, 'Balances.InactiveIssuance')
+    return storageData || BigInt(0)
 }
