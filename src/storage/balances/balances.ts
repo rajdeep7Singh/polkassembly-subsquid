354640d5ed0cb5fe45c2ead91152427b980f2ac2
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-11-07 00:12:31 +0530

    fix

--- src/storage/balances/balances.ts
+++ src/storage/balances/balances.ts
@@ -3,11 +3,23 @@ import { Block, ProcessorContext} from '../../processor'
 
 
 export async function getTotalIssuanceStorageData(ctx: ProcessorContext<Store>, block: Block): Promise<bigint> {
-    const storageData = await block._runtime.getStorage(block.hash, 'Balances.TotalIssuance')
-    return storageData || BigInt(0)
+    try{
+        const storageData = await block._runtime.getStorage(block.hash, 'Balances.TotalIssuance')
+        return storageData || BigInt(0)
+    }
+    catch(e){
+        return BigInt(0)
+    
+    }
 }
 
 export async function getTotalInactiveIssuanceStorageData(ctx: ProcessorContext<Store>, block: Block): Promise<bigint> {
-    const storageData = await block._runtime.getStorage(block.hash, 'Balances.InactiveIssuance')
-    return storageData || BigInt(0)
+    try{
+        const storageData = await block._runtime.getStorage(block.hash, 'Balances.InactiveIssuance')
+        return storageData || BigInt(0)
+    }
+    catch(e){
+        return BigInt(0)
+    
+    }
 }
