1c3eaf245998decf60d36419aa32717a73a107f8
Nikhil Ranjan <niklabh811@gmail.com>
2024-01-31 18:22:08 +0530

    v2700

--- src/mappings/preimageV2/events/preimageNoted.ts
+++ src/mappings/preimageV2/events/preimageNoted.ts
@@ -2,7 +2,8 @@
 import { StorageNotExistsWarn, UnknownVersionError } from '../../../common/errors'
 import {
     statusFor,
-    preimageFor
+    preimageFor,
+    requestStatusFor
 } from '../../../types/preimage/storage'
 
 import { ProposalStatus, ProposalType } from '../../../model'
@@ -67,6 +68,29 @@ export async function getPreimageStatusData(ctx: ProcessorContext<Store>, hash: 
     }
 }
 
+export async function getPreimageRequestStatusData(ctx: ProcessorContext<Store>, hash: string, block: Block): Promise<PreimageStatusStorageData | undefined> {
+    if(requestStatusFor.v2700.is(block)) {
+        const storageData = await requestStatusFor.v2700.get(block, hash)
+        if (!storageData) return undefined
+        if(storageData.__kind == 'Unrequested'){
+            return {
+                status: storageData.__kind,
+                value: storageData.ticket,
+                len: storageData.len
+            }
+        } else{
+            return {
+                status: storageData.__kind,
+                value: storageData.maybeTicket,
+                len: storageData.maybeLen
+            }
+        }
+    }
+    else {
+        throw new UnknownVersionError('preimage.StatusFor')
+    }
+}
+
 export async function handlePreimageV2Noted(ctx: ProcessorContext<Store>,
     item: Event,
     header: any) {
@@ -78,7 +102,7 @@ export async function handlePreimageV2Noted(ctx: ProcessorContext<Store>,
     const hexHash = hash
     const extrinsicIndex = `${header.height}-${item.extrinsicIndex}`
 
-    const storageData = await getStorageData(ctx, hash, header)
+    const storageData = await getPreimageStatusData(ctx, hash, header) || await getPreimageRequestStatusData(ctx, hash, header)
     if (!storageData) {
         ctx.log.warn(StorageNotExistsWarn('PreimageV2', hexHash))
         return
