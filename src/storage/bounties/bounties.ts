d6475cdce29ce90776f85a4524dec40b3eb637bd
alphainfinitus <kumar.mridul000@gmail.com>
2025-05-23 15:14:28 +0530

    refactor: :mute: improve code formatting and structure in bounties.ts

--- src/storage/bounties/bounties.ts
+++ src/storage/bounties/bounties.ts
@@ -2,7 +2,7 @@
 import { UnknownVersionError } from '../../common/errors'
 import { ProcessorContext } from '../../processor'
 import { bounties, bountyDescriptions } from '../../types/bounties/storage'
-import { bounties as TreasuryBountiesStorage, bountyDescriptions as  TreasuryBountyDescriptionsStorage} from '../../types/treasury/storage'
+import { bounties as TreasuryBountiesStorage, bountyDescriptions as TreasuryBountyDescriptionsStorage } from '../../types/treasury/storage'
 
 import { Store } from '@subsquid/typeorm-store'
 
@@ -15,12 +15,14 @@ interface BountyStorageData {
 }
 
 async function getBountyStorageData(ctx: ProcessorContext<Store>, index: number, block: any): Promise<BountyStorageData | undefined> {
-    
+
     if (bounties.v2028.is(block)) {
         return await bounties.v2028.get(block, index)
-    }else if (bounties.v9111.is(block)) {
+    } else if (bounties.v9111.is(block)) {
         return await bounties.v9111.get(block, index)
-    }else {
+    } else if (bounties.v1005000.is(block)) {
+        return await bounties.v1005000.get(block, index)
+    } else {
         throw new UnknownVersionError("Bounty.Bounties")
     }
 }
@@ -29,21 +31,19 @@ async function getTreasuryStorageData(ctx: ProcessorContext<Store>, index: numbe
 
     if (TreasuryBountiesStorage.v2025.is(block)) {
         return await TreasuryBountiesStorage.v2025.get(block, index)
-    }else if (TreasuryBountiesStorage.v2025.is(block)) {
-        return await TreasuryBountiesStorage.v2025.get(block, index)
-    }else {
+    } else {
         throw new UnknownVersionError("Treasury.Bounties")
     }
 }
 
 export async function getBounties(ctx: ProcessorContext<Store>, index: number, block: any) {
     let bountyInfo;
-    try{
+    try {
         bountyInfo = await getBountyStorageData(ctx, index, block)
-    }catch {
+    } catch {
         bountyInfo = await getTreasuryStorageData(ctx, index, block)
     }
-    if(!bountyInfo) return undefined;
+    if (!bountyInfo) return undefined;
     let description = await getDescription(ctx, index, block).then((r) => r || '');
     return {
         ...bountyInfo,
@@ -70,7 +70,7 @@ async function getTreasuryDescriptionStorageData(ctx: ProcessorContext<Store>, i
 async function getDescription(ctx: ProcessorContext<Store>, index: number, block: any) {
     try {
         return await getBountyDescriptionStorageData(ctx, index, block)
-    }catch {
+    } catch {
         await getTreasuryDescriptionStorageData(ctx, index, block)
     }
 }
